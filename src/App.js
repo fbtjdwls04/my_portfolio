6986d85c31860385ccb1d875690a8a4a8b021d6e
fbtjdwls04 <fbtjdwls04@gmail.com>
2023-06-14 17:00:48 +0900

    스크롤 이벤트 코드 단축

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import { Tabs, Tab } from "@mui/material";
 import AboutPage from "./pages/AboutPage";
 import WorksPage from "./pages/WorksPage";
 import HomePage from "./pages/HomePage";
-import IntroPage from "./pages/IntroPage";
+import StackPage from "./pages/StackPage";
 import { useEffect, useState, useRef } from "react";
 export default function App() {
-  const page1 = "/home";
-  const page2 = "/intro";
-  const page3 = "/about";
-  const page4 = "/works";
+  const page1 = "/1";
+  const page2 = "/2";
+  const page3 = "/3";
+  const page4 = "/4";
   const [value, SetValue] = useState(1);
   const locate = useLocation();
   const navigate = useNavigate();
@@ -34,19 +34,7 @@ export default function App() {
   // Tab이나 url로 라우팅시 Tab의 바텀바가 따라옴
   const pageChange = (value) => {
     SetValue(value);
-    if (value == 1) {
-      navigate(page1);
-      scrollMove(0);
-    } else if (value == 2) {
-      navigate(page2);
-      scrollMove(pageHeight);
-    } else if (value == 3) {
-      navigate(page3);
-      scrollMove(pageHeight * 2);
-    } else if (value == 4) {
-      navigate(page4);
-      scrollMove(pageHeight * 3);
-    }
+    navigate("/" + value);
   };
 
   // 스크롤 조작 시 한 화면씩 움직이기
@@ -56,53 +44,20 @@ export default function App() {
     const wheelHandler = (e) => {
       e.preventDefault();
       const { deltaY } = e;
-      const { scrollTop } = outerDivRef.current; // 스크롤 위쪽 끝부분 위치
       if (deltaY > 0) {
         // 스크롤 내릴 때
-        if (scrollTop >= 0 && scrollTop < pageHeight) {
-          //현재 1페이지
-          scrollMove(pageHeight);
-          SetValue(2);
-          navigate(page2);
-        } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
-          //현재 2페이지
-          scrollMove(pageHeight * 2);
-          SetValue(3);
-          navigate(page3);
-        } else if (scrollTop >= pageHeight * 2 && scrollTop < pageHeight * 3) {
-          //현재 3페이지
-          scrollMove(pageHeight * 3);
-          SetValue(4);
-          navigate(page4);
-        } else {
-          // 현재 4페이지
-          scrollMove(pageHeight * 3);
+        if (value < 4) {
+          navigate("/" + (value + 1));
         }
       } else {
-        // 스크롤 올릴 때
-        if (scrollTop >= 0 && scrollTop < pageHeight) {
-          //현재 1페이지
-          scrollMove(0);
-        } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
-          //현재 2페이지
-          scrollMove(0);
-          SetValue(1);
-          navigate(page1);
-        } else if (scrollTop >= pageHeight * 2 && scrollTop < pageHeight * 3) {
-          //현재 3페이지
-          scrollMove(pageHeight);
-          SetValue(2);
-          navigate(page2);
-        } else {
-          // 현재 4페이지
-          scrollMove(pageHeight * 2);
-          SetValue(3);
-          navigate(page3);
+        if (value > 1) {
+          navigate("/" + (value - 1));
         }
       }
     };
     const outerDivRefCurrent = outerDivRef.current;
     outerDivRefCurrent.addEventListener("wheel", wheelHandler);
+    // 주소창에 직접 입력시 스크롤 이동
     if (locate.pathname == page1) {
       SetValue(1);
       scrollMove(0);
@@ -149,18 +104,10 @@ export default function App() {
           <Tab value={4} label="WORKS" disableRipple />
         </Tabs>
       </div>
-
-      <Routes>
-        <Route path={page1}></Route>
-        <Route path="*"></Route>
-        <Route path={page2}></Route>
-        <Route path={page3}></Route>
-        <Route path={page4}></Route>
-      </Routes>
       <div ref={outerDivRef} id="outer">
         <HomePage />
-        <IntroPage />
         <AboutPage />
+        <StackPage />
         <WorksPage />
       </div>
     </div>
